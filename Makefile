7560b23602cc0af40b77b20190d6901e57649251
Gustav Behm <me@rootmos.io>
2019-08-01 10:36:07 +0200

    Refactor to use ppx and errors in the Lwt monad

--- Makefile
+++ Makefile
@@ -2,12 +2,12 @@ run: ping
 	sudo strace -e trace=network ./$<
 
 %: %.ml
-	ocamlfind opt -linkpkg -package lwt.unix -o $@ $^
+	ocamlfind opt -linkpkg -package lwt.unix -package lwt_ppx -o $@ $^
 
 clean:
 	rm -rf ping *.cmi *.cmo
 
 deps:
-	opam install ocamlfind lwt
+	opam install ocamlfind lwt lwt_ppx merlin
 
 .PHONY: run clean deps
--- ping.ml
+++ ping.ml
@@ -5,7 +5,7 @@ open Printf
 let string_of_sockaddr = function
 | ADDR_INET (a, 0) -> sprintf "%s" (Unix.string_of_inet_addr a)
 | ADDR_INET (a, p) -> sprintf "%s:%d" (Unix.string_of_inet_addr a) p
-| _ -> failwith "unexpected address format"
+| ADDR_UNIX s -> s
 
 let target = "ip.rootmos.io"
 
@@ -16,10 +16,11 @@ let next_seq () = let s = !seq in incr seq; s
 
 let max_len = 576
 
-let t = gethostbyname target >|= fun he ->
+let t =
+  let%lwt he = gethostbyname target in
   match Array.length he.h_addr_list with
-  | 0 -> failwith (sprintf "no such host %s" target)
-  | _ -> ADDR_INET (Array.get he.h_addr_list 0, 0)
+  | 0 -> fail_with (sprintf "no such host %s" target)
+  | _ -> return @@ ADDR_INET (Array.get he.h_addr_list 0, 0)
 
 let checksum_1071 bs =
   let sum = ref 0 and count = ref @@ Bytes.length bs and off = ref 0 in
@@ -37,8 +38,8 @@ let checksum_1071 bs =
 let ping_req payload =
   let open Bytes in
   let l = 8 + length payload in
-  if l > max_len then invalid_arg "payload too big";
-  let msg = create l in
+  if l > max_len then fail_invalid_arg "payload too big"
+  else let msg = create l in
   set_uint8 msg 0 8; (* type *)
   set_uint8 msg 1 0; (* code *)
   set_uint16_ne msg 2 0; (* checksum *)
@@ -46,27 +47,29 @@ let ping_req payload =
   set_uint16_ne msg 6 @@ next_seq (); (* sequence *)
   blit payload 0 msg 8 @@ length payload;
   set_uint16_ne msg 2 (checksum_1071 msg |> lnot);
-  msg
+  return msg
 
 let s = socket PF_INET SOCK_RAW 1
 
 let send t =
-  let req = ping_req (Bytes.of_string "hello") in
-  sendto s req 0 (Bytes.length req) [] t >|= fun sent ->
-    if sent <> (Bytes.length req) then failwith "unable to send whole ping request"
+  let%lwt req = ping_req (Bytes.of_string "hello") in
+  let%lwt sent = sendto s req 0 (Bytes.length req) [] t in
+  if sent <> (Bytes.length req)
+  then fail_with "unable to send whole ping request"
+  else return ()
 
 let recv () =
   let rec go () =
     let open Bytes in
     let msg = create max_len in
-    recvfrom s msg 0 (length msg) [] >>= fun (l, a) ->
-      let off = 4 * (get_uint8 msg 0 land 0x0f) in
-      let id = get_uint16_ne msg (off + 4) in
-      if id <> identifier then go () else
-        let seq = get_uint16_ne msg (off + 6) in
-        printf "received %d bytes (seq %d) from %s\n"
-          (l - off) seq (string_of_sockaddr a);
-        return ()
+    let%lwt (l, a) = recvfrom s msg 0 (length msg) [] in
+    let off = 4 * (get_uint8 msg 0 land 0x0f) in
+    let id = get_uint16_ne msg (off + 4) in
+    if id <> identifier then go () else
+      let seq = get_uint16_ne msg (off + 6) in
+      printf "received %d bytes (seq %d) from %s\n"
+        (l - off) seq (string_of_sockaddr a);
+      return ()
   in with_timeout 1.0 go
 
 let () = Lwt_main.run (t >>= send <&> recv ())
